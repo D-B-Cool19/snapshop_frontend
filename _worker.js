4516d54f8af706c30ae59e19b764c40793ce313d
Cheng <Chengchenghu0728@gmail.com>
2024-10-10 20:24:25 +0800

    ⚗️ 调试Cloudflare代理

--- _worker.js
+++ _worker.js
@@ -3,14 +3,15 @@ export default {
         const url = new URL(request.url);
         console.log('Request received for:', url.pathname);
         if (url.pathname.startsWith('/api/')) {
-            const apiUrl = `http://47.98.118.134${url.pathname}`;
-            console.log('Proxying request to:', apiUrl);
-            const newRequest = new Request(apiUrl, {
-                method: request.method,
-                headers: request.headers,
-                body: request.body,
-            });
-            return env.ASSETS.fetch(newRequest);
+            // const apiUrl = `http://47.98.118.134${url.pathname}`;
+            // console.log('Proxying request to:', apiUrl);
+            // const newRequest = new Request(apiUrl, {
+            //     method: request.method,
+            //     headers: request.headers,
+            //     body: request.body,
+            // });
+            // return env.ASSETS.fetch(newRequest);
+            return new Response('Ok');
         }
         return env.ASSETS.fetch(request);
     },
