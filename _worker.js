0a107fff930eb066dc61d536f06b045afd282ae8
Cheng <Chengchenghu0728@gmail.com>
2024-10-10 18:52:36 +0800

    🔊 添加Debug信息

--- _worker.js
+++ _worker.js
@@ -1,8 +1,10 @@
 export default {
     async fetch(request) {
         const url = new URL(request.url);
+        console.log('Request received for:', url.pathname);
         if (url.pathname.startsWith('/api/')) {
             const apiUrl = `http://47.98.118.134${url.pathname}`;
+            console.log('Proxying request to:', apiUrl);
             const newRequest = new Request(apiUrl, {
                 method: request.method,
                 headers: request.headers,
